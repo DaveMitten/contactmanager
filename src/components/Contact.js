c89a34e6cb5298e9efde10e1930f1f8cfa530119
David Mitten <davidmitten88@gmail.com>
2018-09-04 16:00:19 +0100

    wip

--- src/components/Contact.js
+++ src/components/Contact.js
@@ -3,34 +3,47 @@ import PropTypes from "prop-types";
 
 class Contact extends Component {
   state = {
-    showContactInfo: true
+    showContactInfo: false
   };
 
   onShowClick = e => {
     
   };
-
+onDeleteClick= () => {
+  console.log('clicked');
+  this.props.deleteClickHandler();
+}
   render() {
     const { name, email, phone } = this.props.contact;
+    const { showContactInfo } = this.state;
     return (
       <div className="card card-body mb-3">
         <h4>
           {name} <i onClick=
           {()=> {this.setState ({ 
       showContactInfo: !this.state.showContactInfo });}} 
-      className="fas fa-sort-down" />
+      className="fas fa-sort-down" 
+      style={{ cursor: 'pointers'}}
+      />
+      <i className="fas fa-times" style={{
+        cursor: 'pointer', float: 'right', color: 'red'}}
+        onClick={this.onDeleteClick}>
+        </i>
         </h4>
-        <ul className="list-group">
+        { showContactInfo ? (<ul className="list-group">
           <li className="list-group-item"> Email: {email}</li>
           <li className="list-group-item"> Phone: {phone}</li>
-        </ul>
+        </ul>)
+        : null }
+        
       </div>
     );
   }
 }
 
 Contact.propTypes = {
-  contact: PropTypes.object.isRequired
+  contact: PropTypes.object.isRequired,
+  deleteClickHandler: PropTypes.func.isRequired
 };
 
 export default Contact;
--- src/components/Contacts.js
+++ src/components/Contacts.js
@@ -2,47 +2,54 @@ import React, { Component } from "react";
 import Contact from "./Contact";
 
 class Contacts extends Component {
-   state = {
-      contacts: [
-        {
-          id: 1,
-          name: "John Doe",
-          email: "Jdoe@gmail.com",
-          phone: "111-111-11111"
-        },
-        {
-          id: 2,
-          name: "Karen Williams",
-          email: "KWilliams@gmail.com",
-          phone: "222-222-22222"
-        },
-        {
-          id: 3,
-          name: "Henry Johnson",
-          email: "Henry@gmail.com",
-          phone: "333-333-33333"
-        }
-      ]
-    };
-  
+  state = {
+    contacts: [
+      {
+        id: 1,
+        name: "John Doe",
+        email: "Jdoe@gmail.com",
+        phone: "111-111-11111"
+      },
+      {
+        id: 2,
+        name: "Karen Williams",
+        email: "KWilliams@gmail.com",
+        phone: "222-222-22222"
+      },
+      {
+        id: 3,
+        name: "Henry Johnson",
+        email: "Henry@gmail.com",
+        phone: "333-333-33333"
+      }
+    ]
+  };
+
+  deleteContact = id => {
+    // console.log(id);
+    const { contacts } = this.state;
+    const newContacts = contacts.filter(contact => contact.id !== id);
+
+    this.setState({
+      contacts: newContacts
+    });
+  };
+
   render() {
     const { contacts } = this.state;
 
     return (
       <React.Fragment>
-        {contacts.map(contact => (  
-            
-        <Contact 
-    
-        key= {contact.id}
-        contact= { contact }/>        
-        
+        {contacts.map(contact => (
+          <Contact
+            key={contact.id}
+            contact={contact}
+            deleteClickHandler={this.deleteContact.bind(this, contacts.id)}
+          />
         ))}
       </React.Fragment>
     );
   }
-
 }
- 
 
 export default Contacts;
