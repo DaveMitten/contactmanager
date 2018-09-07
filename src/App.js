4e6857127eb6336cf8f205ed7da4c749cf5236bf
David Mitten <davidmitten88@gmail.com>
2018-09-07 08:10:17 +0100

    got 2 steps further WIP

--- src/App.js
+++ src/App.js
@@ -1,23 +1,26 @@
-import React, { Component } from 'react';
-import Contacts from './components/Contacts';
-import Header from './components/Header';
+import React, { Component } from "react";
+import Contacts from "./components/contacts/Contacts";
+import AddContact from "./components/contacts/AddContact";
 
-import { Provider } from './context';
+import Header from "./components/layout/Header";
 
-import 'bootstrap/dist/css/bootstrap.min.css';
+import { Provider } from "./context";
 
-import './App.css';
+import "bootstrap/dist/css/bootstrap.min.css";
+
+import "./App.css";
 
 class App extends Component {
   render() {
     return (
       <Provider>
-      <div className="App">
-        <Header branding="Contact Manager" />
-
-        <Contacts />
-
-      </div>
+        <div className="App">
+          <Header branding="Contact Manager" />
+          <div className="container">
+            <AddContact />
+            <Contacts />
+          </div>
+        </div>
       </Provider>
     );
   }
